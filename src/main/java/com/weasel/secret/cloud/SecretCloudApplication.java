dc6cf02c08055d7442f4575d24b44c1055e5e51c
pickear <pickear@gmail.com>
2017-11-08 20:46:48 +0800

    init

---
+++ src/main/java/com/weasel/secret/cloud/SecretCloudApplication.java
@@ -0,0 +1,12 @@
+package com.weasel.secret.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretCloudApplication.class, args);
+	}
+}
