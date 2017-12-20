65f9c687a3c11f25ef3e5d8be762fcab9141d605
pickear <pickear@gmail.com>
2017-12-20 11:29:01 +0800

    回方法

--- src/main/java/com/weasel/secret/cloud/infrastructure/helper/GsonHelper.java
+++ src/main/java/com/weasel/secret/cloud/infrastructure/helper/GsonHelper.java
@@ -2,6 +2,9 @@ package com.weasel.secret.cloud.infrastructure.helper;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
 
 /**
  * Created by dell on 2017/11/23.
@@ -28,6 +31,16 @@ public final class GsonHelper {
      */
     public static <T> T fromJson(String json,Class<T> clazz){
         return gson.fromJson(json,clazz);
+    }
+
+    /**
+     *
+     * @param json
+     * @param <T>
+     * @return
+     */
+    public static <T> T fromJson(String json,TypeToken<T> typeToken){
+        return gson.fromJson(json,typeToken.getType());
     }
 
     protected GsonHelper(){}
